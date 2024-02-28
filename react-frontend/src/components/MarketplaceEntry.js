1f05701340c8cd033040d63634367c94521b9a19
jestra03 <joshemestra@gmail.com>
2024-02-28 13:14:37 -0800

    some visual changes, text overflow, re-sizing entries

--- react-frontend/src/components/MarketplaceEntry.js
+++ react-frontend/src/components/MarketplaceEntry.js
@@ -4,6 +4,16 @@ import missing from '../assets/missing.jpg';
 import { Link } from 'react-router-dom';
 
 function Entry({title, price, listingID }) {
+  const getTitleFontSize = () => {
+    if (title.length > 21) {
+      return '15px';
+    } else if (title.length > 14) {
+      return '20px';
+    } else {
+      return '25px';
+    }
+  }
+
   return (
     <Link to={`/listings/${listingID}`} className="entry-link">
       <div className="entry-container">
@@ -12,7 +22,7 @@ function Entry({title, price, listingID }) {
         </div>
 
         <div className="text-container">
-          <h2 className="title">{title}</h2>
+          <h2 className="title" style = {{fontSize: getTitleFontSize()}}>{title}</h2>
           <p className="price">{price === "0.00" ? "FREE" : `$${price}`}</p>
         </div>
       </div>
