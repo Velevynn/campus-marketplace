4cba34dfde9b7a0d18bb0fcc26171a98dcb092e9
jestra03 <joshemestra@gmail.com>
2024-03-04 11:48:41 -0800

    fix desc box

--- react-frontend/src/components/Form.js
+++ react-frontend/src/components/Form.js
@@ -101,7 +101,7 @@ function Form() {
           onChange={handleChange}
         />
         <label htmlFor="description">Description</label>
-        <input
+        <textarea
           type="text"
           name="description"
           id="description"
