1486253203076fc1d601574a8bce80acccf27e81
jestra03 <joshemestra@gmail.com>
2024-03-03 16:59:03 -0800

    protected routing added

---
+++ react-frontend/src/utils/ProtectedRoute.js
@@ -0,0 +1,18 @@
+// ProtectedRoute.js
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({children}) => {
+    const isAuthenticated = localStorage.getItem('token') !== null;
+    if (!isAuthenticated) {
+        return <Navigate to = "/login"/>;
+    } 
+    return children;
+}
+
+ProtectedRoute.propTypes = {
+    children: PropTypes.node.isRequired // Ensure children prop is provided and is a node
+};
+
+export default ProtectedRoute;
