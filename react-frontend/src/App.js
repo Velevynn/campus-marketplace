13cd5290e886d7b32b741a24865c38b1ac8603cc
jestra03 <joshemestra@gmail.com>
2024-03-06 13:32:35 -0800

    aesthetic changes (marketplace)

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SignUpPage from "./Authentication/SignUpPage";
-import LoginPage from './Authentication/LoginPage';
-import ProfilePage from './Profile/ProfilePage.js';
+import SignUpPage from "./authentication/SignUpPage.js";
+import LoginPage from './authentication/LoginPage.js';
+import ProfilePage from './profile/ProfilePage.js';
 import Marketplace from "./pages/Marketplace";
 import AddListing from "./pages/AddListing";
 import NavBar from "./components/NavBar";
 import About from "./pages/About";
 import TOS from "./pages/TOS";
-import BuyerListingView from "./Listings/BuyerView";
-import SellerListingView from "./Listings/SellerView";
+import BuyerListingView from "./listings/BuyerView.js";
+import SellerListingView from "./listings/SellerView.js";
 import ProtectedRoute from "./utils/ProtectedRoute"; // Importing ProtectedRoute
 
 function App() {
