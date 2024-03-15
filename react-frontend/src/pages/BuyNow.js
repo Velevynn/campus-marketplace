3c3188f14ba36a9eb41ffb14250b0a8cd1439cf7
jcromeen <jimmy.cromeenes@gmail.com>
2024-03-15 15:21:00 -0700

    tried to combine seller and buyer view

---
+++ react-frontend/src/pages/BuyNow.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BuyNow(){
+    return (
+        <div className="listing-container">
+            <h3> BUY NOW </h3>
+        </div>
+    );
+};
+
+export default BuyNow;
---
+++ react-frontend/src/pages/EditListing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EditListing(){
+    return (
+        <div className="listing-container">
+            <h3> EDIT LISTING </h3>
+        </div>
+    );
+};
+
+export default EditListing;
