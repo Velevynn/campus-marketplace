2d6c390360eaccc6d8c053424cb108d8503e18b2
Velevynn <afspecht02@gmail.com>
2024-03-15 18:53:26 -0700

    other cors options

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const listingRoutes = require('./routes/listingRoutes');
@@ -7,16 +6,16 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 200 // Set the status code for successful OPTIONS requests
+  };
 
-app.use(cors({
-    origin: 'localhost:3000',
-}));
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
 
 app.use('/users', userRoutes);  // user-related routes are in listingRoutes.js
 app.use('/listings', listingRoutes);  // listing-related routes are handled in userRoutes.js
