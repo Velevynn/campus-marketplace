7a5db2b60d2ae9eb43d9e782d8b076939be4fbc8
jestra03 <joshemestra@gmail.com>
2024-03-18 05:37:55 -0700

    UI changes

---
+++ react-frontend/src/components/LoadingSpinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LoadingSpinner.css'; // Import your CSS file for styling the spinner
+
+function LoadingSpinner() {
+    console.log("hello");
+  return (
+    <div className="loading-spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+}
+
+export default LoadingSpinner;
