ae82311ffa1c514c21f0b00e3e3064fcd5610a7f
jestra03 <joshemestra@gmail.com>
2024-03-17 21:54:28 -0700

    bios

--- react-frontend/src/pages/About.js
+++ react-frontend/src/pages/About.js
@@ -10,36 +10,28 @@ function About() {
       <div className="team-container">
         <Bio
           name="Alex"
-          bio="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
-        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+          bio="Karan likes miget"
         />
 
         <Bio
           name="Alexander"
-          bio="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
-        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+          bio="lets dance"
         />
 
         <Bio
           name="Joshua"
-          bio="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
-        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+          bio="fucc"
         />
 
         <Bio
           name="Jimmy"
           bio="
-          Hi there, I'm Jimmy, a third-year computer science major at Cal Poly. I thrive on the challenges 
-          of coding and problem-solving, both in and out of the classroom. Beyond my studies, I'm always 
-          diving into creative tech projects, driven by a passion to make a mark in the ever-evolving world 
-          of technology. My goal? To contribute innovatively and shape the future through my love for 
-          coding and exploration of cutting-edge solutions."
+          pro valorant didnt shwer"
         />
 
         <Bio
           name="Karan"
-          bio="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
-        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+          bio="yo (im cooked)"
         />
       </div>
     </div>
