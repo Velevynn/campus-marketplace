66ce17322eaaaca83138ca4f93cf68c025a59a2f
jestra03 <joshemestra@gmail.com>
2024-04-25 12:22:51 -0700

    fixed profile page

--- react-frontend/src/profile/ProfilePage.js
+++ react-frontend/src/profile/ProfilePage.js
@@ -42,7 +42,7 @@ function ProfilePage() {
     try {
       const response = await axios.get(`https://haggle.onrender.com/users/profile`, {
         headers: {
-          'Authorization': `Bearer ${token}`console.log(userID);
+          'Authorization': `Bearer ${token}`,
         }
       });
       setUserProfile(response.data);
