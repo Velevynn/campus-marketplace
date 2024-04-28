bb883271b6a3cb867a27e661cf49ec6754f3bf06
Alex Zaharia <alexcz@outlook.com>
2024-04-28 14:12:52 -0700

    restored login...old login in backup folder

--- react-frontend/src/backup/LoginPage.js
+++ react-frontend/src/backup/LoginPage.js
@@ -2,12 +2,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 // Importing navigation hooks and components for routing and navigation
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 // Importing logo, icons, and styled components for UI
 import logoImage from '../assets/haggle-horizontal.png';
 import { FaEye, FaEyeSlash  } from 'react-icons/fa';
-import { Container, Form, LogoImage, ErrorLabel, InputGroup, Input, InputLabel, HeaderLabel, VisibilityToggle, Button, GoogleImage, LinkedLabel, ForgotPasswordLabel, BottomContainer, BottomLabel } from './AuthenticationStyling';
 import googlepng from '../assets/google.png';
+import "./LoginPage.css"
+
+
 // LoginPage component for handling user login
 function LoginPage() {
   // State for storing user credentials, form validity, error messages, and password visibility
@@ -15,16 +17,19 @@ function LoginPage() {
   const [isFormValid, setIsFormValid] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const [passwordVisible, setPasswordVisible] = useState(false);
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
+
   // Handles changes in input fields and updates the credentials state
   const handleChange = (event) => {
     const { name, value } = event.target;
     setCredentials({ ...credentials, [name]: value });
   };
+
   // Toggles the visibility of the password input field
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
+
   // Handles the form submission event for login
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -35,8 +40,8 @@ function LoginPage() {
         password: credentials.password,
       };
       const response = await axios.post('https://haggle.onrender.com/users/login', requestBody);
-      localStorage.setItem('token', response.data.token); // Stores the received token in local storage and navigates to the profile page
-      window.location.href = '/profile';
+      localStorage.setItem('token', response.data.token); // Stores the received token
+      navigate('/profile');
     } catch (error) {
       // Sets an error message based on the response from the server or a general failure message
       if (error.response) {
@@ -46,12 +51,15 @@ function LoginPage() {
       }
     }
   };
+
   const handleGoogleLogin = () => {
     const clientId = '71122616560-tv80mel7fi0s2etitj1enhk192v06h0e.apps.googleusercontent.com';
+
     const redirectUrl = 'https://haggle.onrender.com/users/auth/google/callback';
     const scope = encodeURI('email profile');
     const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUrl}&response_type=code&scope=${scope}&access_type=offline&prompt=consent`;
     window.location.href = authUrl;
+    
   };
 
   // Effect hook to update the form validity based on the credentials state
@@ -59,79 +67,119 @@ function LoginPage() {
     const isValid = credentials.identifier.length > 0 && credentials.password.length > 0;
     setIsFormValid(isValid);
   }, [credentials]);
+
+  useEffect(() => {
+    // This function would be called on a component that captures the redirect.
+    const handleAuthCallback = async () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const code = urlParams.get('code');
+        if (code) {
+            try {
+                const response = await axios.get(`/api/auth/google/callback?code=${code}`);
+                localStorage.setItem('token', response.data.token);
+                navigate('/profile');
+            } catch (error) {
+                console.error('Error handling auth callback:', error);
+            }
+        }
+    };
+
+    handleAuthCallback();
+}, [navigate]);
+
   // Renders the login form, providing fields for identifier and password, and displays error messages if any exist
   return (
-    <>
-      <Container>
-        <LogoImage src={logoImage} alt="Logo"/>
+    <div className="vertical-center margin">
+      <div>
+      <div className="small-container drop-shadow">
+        <div className="vertical-center">
+          <img className="logo-img" src={logoImage} alt="Logo"/>
+        </div>
         
-        <HeaderLabel style={{ marginTop: '0px'}}>
+        <h5 className="text-center" style={{fontSize: "18px"}}>
             Log in to buy, sell, and trade
-        </HeaderLabel>
-        <Form onSubmit={handleSubmit}>
+        </h5>
+
+        <form onSubmit={handleSubmit}>
           {errorMessage && (
-            <ErrorLabel>
+            <p className="margin" style={{color: "red", fontSize: "12px"}}>
               {errorMessage}
-            </ErrorLabel>
+            </p>
           )}
-          <InputGroup>
-            <InputLabel htmlFor="identifier" hasContent={credentials.identifier.length > 0}>Email, Phone, or Username</InputLabel>
-            <Input
+          <div className="margin input">
+            <input
               type="text"
               name="identifier"
               id="identifier"
               value={credentials.identifier}
               onChange={handleChange}
-              hasContent={credentials.identifier.length > 0}
+              placeholder="Email, Phone, or Username"
+              autoComplete="on"
               required/>
-          </InputGroup>
-          <InputGroup>
-            <InputLabel htmlFor="password" hasContent={credentials.password.length > 0}>Password</InputLabel>
-            <Input
+          </div>
+          <div className="margin input">
+            <input
               type={passwordVisible ? "text" : "password"}
               name="password"
               id="password"
               value={credentials.password}
               onChange={handleChange}
-              hasContent={credentials.password.length > 0}
+              placeholder="Password"
+              autoComplete="current-password"
+              style={{paddingRight: "2.5rem"}}
               required/>
-            <VisibilityToggle onClick={togglePasswordVisibility}>
+          
+            <div className="toggle-icon" onClick={togglePasswordVisibility}>
                 {passwordVisible ? <FaEye /> : <FaEyeSlash />}
-            </VisibilityToggle>
-          </InputGroup>
-          <Button type="submit" disabled={!isFormValid}>
-              Log in
-          </Button>
-          <Button onClick={handleGoogleLogin} style={{ background: '#14A44A' , padding: '6px'}}>
-            <GoogleImage src={googlepng} alt="google"></GoogleImage>
-            <span style={{ position: 'relative', top: '2px' }}>Continue with Google</span>
-          </Button>
-          <LinkedLabel>
-              By logging in you agree to our {}
-                <Link to="/terms-of-service" style={{ display: 'inline', color: '#0056b3', fontWeight: 'bold'}}>
-                  Terms of Service
-                </Link>
-              {} and acknowledge our {}
-                <Link to="/privacy-policy" style={{ display: 'inline', color: '#0056b3', fontWeight: 'bold'}}>
-                  Privacy Policy
-                </Link>
-            </LinkedLabel>
-            <ForgotPasswordLabel>
-            <Link to="/forgot-password" style={{ display: 'inline', color: '#0056b3'}}>
+            </div>
+          </div>
+          
+            
+          <div className="margin">
+          <button className={`span-button ${isFormValid ? "" : "disabled"}`} type="submit" disabled={!isFormValid}>
+            Log in
+          </button>
+          </div>
+
+          <div className="margin">
+            <button className="span-button margin" onClick={handleGoogleLogin}>
+              <div className="vertical-center" >
+                <img className="google-img" src={googlepng} alt="google"></img>
+                <span className="margin-center">Continue with Google</span>
+              </div>
+            </button>
+          </div>
+
+          <p className="text-center margin-bottom" style={{fontSize: '12px'}}>
+            By logging in you agree to our {}
+              <Link to="/terms-of-service" >
+                Terms of Service
+              </Link>
+            {} and acknowledge our {}
+              <Link to="/privacy-policy">
+                Privacy Policy
+              </Link>
+          </p>
+
+          <p className="text-center">
+            <Link to="/forgot-password">
               Forgot password?
             </Link>
-          </ForgotPasswordLabel>
-        </Form>
-      </Container>
-      <BottomContainer>
-        <BottomLabel>
+          </p>
+        </form>
+      </div>
+
+      <div className="small-container drop-shadow margin-top">
+        <p className="text-center">
           Don&apos;t have an account? {}
-          <Link to="/signup" style={{ display: 'inline', color: '#0056b3', fontWeight: 'bold'}}>
+          <Link to="/signup">
             Sign up
           </Link>
-        </BottomLabel>
-      </BottomContainer>
-    </>
+        </p>
+      </div>
+      </div>
+    </div>
   );
 }
+
 export default LoginPage;
