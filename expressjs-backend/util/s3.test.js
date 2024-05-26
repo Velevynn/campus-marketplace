301884b16bacd0ba98b88c2220da9f35b06688a6
realkaranvir <113927390+realkaranvir@users.noreply.github.com>
2024-05-26 16:09:49 -0700

    added describe blocks to s3 tests

--- expressjs-backend/util/s3.test.js
+++ expressjs-backend/util/s3.test.js
@@ -10,28 +10,35 @@ afterAll(async () => {
     await s3.deleteS3Folder("testing");
 });
 
-test("Test successful file upload", async () => {
-    const imageName = "testing/testImage";
-    const imageData = "testimagedata";
-    //Returns the AWS response which is the link to the uploaded file if success
-    const response = await s3.uploadImageToS3(imageName, imageData);
-    
-    //Ensure link is reflective of uploaded file
-    expect(response).toEqual(`https://haggleimgs.s3.amazonaws.com/${imageName}`);
-});
+
+describe("file upload", () => {
+
+    test("Test successful file upload", async () => {
+        const imageName = "testing/testImage";
+        const imageData = "testimagedata";
+        //Returns the AWS response which is the link to the uploaded file if success
+        const response = await s3.uploadImageToS3(imageName, imageData);
+        
+        //Ensure link is reflective of uploaded file
+        expect(response).toEqual(`https://haggleimgs.s3.amazonaws.com/${imageName}`);
+    });
 
 
-test("Test unsuccessful file upload(null image data", async () => {
-    const imageName = "testing/testImage";
-    const imageData = null;
-    await expect(s3.uploadImageToS3(imageName, imageData)).rejects.toThrow();
-});
+    test("Test unsuccessful file upload(null image data", async () => {
+        const imageName = "testing/testImage";
+        const imageData = null;
+        await expect(s3.uploadImageToS3(imageName, imageData)).rejects.toThrow();
+    });
 
-test("Test unsuccessful file upload(null image data", async () => {
-    const imageName = null;
-    const imageData = "imagedata";
-    await expect(s3.uploadImageToS3(imageName, imageData)).rejects.toThrow();
-});
+    test("Test unsuccessful file upload(null image data", async () => {
+        const imageName = null;
+        const imageData = "imagedata";
+        await expect(s3.uploadImageToS3(imageName, imageData)).rejects.toThrow();
+    });
+
+})
+
+describe("object listing", () => {
 
 test("Test Successful listing of objects", async () => {
     //Upload testing file
@@ -43,6 +50,9 @@ test("Test Successful listing of objects", async () => {
     expect(testFiles.length > 0).toEqual(true);
 });
 
+})
+
+describe("image deletion", () => {
 
 test("Test successful image deletion", async () => {
     //Upload testing file
@@ -59,6 +69,10 @@ test("Test successful image deletion", async () => {
     //Expect listed files to be empty
     expect(testFiles).toEqual([]);
 });
+
+})
+
+describe("object renaming", () => {
 
 test("Test renaming s3 object", async () => {
     //Upload testing file
@@ -80,3 +94,5 @@ test("Test renaming s3 object", async () => {
     expect(testFiles[0].Key).toEqual(newImageName);
 
 });
+
+})
