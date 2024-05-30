fa556c64ecba3691921ff2976b6c17fca003d080
jestra03 <joshemestra@gmail.com>
2024-05-30 13:15:35 -0700

    added visual improvements for marketplace, search query is presented and No Search Results prompt shown if no results found

--- react-frontend/src/pages/Marketplace.js
+++ react-frontend/src/pages/Marketplace.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useLocation } from "react-router-dom";
 import Entry from "../components/MarketplaceEntry";
 import ScrollToTopButton from "../components/ScrollToTopButton";
 import LoadingSpinner from "../components/LoadingSpinner"; // Import the loading spinner component
+import { MdNightlightRound } from 'react-icons/md';
 
 function Marketplace() {
   const [searchParams] = useSearchParams();
@@ -11,6 +12,17 @@ function Marketplace() {
   const [entries, setEntries] = useState([]);
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true); // Add loading state
+  const [query, setQuery] = useState('');
+  const location = useLocation();
+
+  const getQueryFromURL = () => {
+    const search = location.search;  // save query string
+    if (search.startsWith('?q=')) {
+      const queryString = search.substring(3);  // truncate to remove '?q='
+      return decodeURIComponent(queryString);  // converts ASCII codes for spaces and other special characters
+    }
+    return '';  // return nothing if there's no query
+  }
 
   if (q === null) {
     q = "";
@@ -18,7 +30,10 @@ function Marketplace() {
 
   useEffect(() => {
     fetchEntries();
-  }, [q, page]);
+    const initialQuery = getQueryFromURL();
+    setQuery(initialQuery);
+    console.log("hello: ", query);
+  }, [q, page, location]);
 
   async function fetchEntries() {
     try {
@@ -53,11 +68,21 @@ function Marketplace() {
 
   return (
     <div>
+      <div className="left-container">
+        <h1>{query}</h1>
+      </div>  
       <div className="full-container">
         {isLoading ? ( // Render loading spinner if isLoading is true
           <div className="margin">
             <LoadingSpinner />
           </div>
+        ) : entries.length === 0 ? (
+          <div>
+          <MdNightlightRound style={{ marginBottom: '-30px', color: 'black', marginTop:'10px'}} size={40}/>
+          <h2>
+            No Search Results  
+          </h2>
+        </div>
         ) : (
           entries.map((entry) => (
             <Entry 
@@ -68,6 +93,7 @@ function Marketplace() {
             />
           ))
         )}
+
       </div>
       <ScrollToTopButton onClick={scrollToTop} />
     </div>
