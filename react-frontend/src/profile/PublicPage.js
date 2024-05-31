fcf127be409ee95f65bfd1a354326a8db96dc28b
jestra03 <joshemestra@gmail.com>
2024-05-30 23:27:21 -0700

    loading state provided to profile view

--- react-frontend/src/profile/PublicPage.js
+++ react-frontend/src/profile/PublicPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './profile.css';
-//import LoadingSpinner from "../components/LoadingSpinner";
+import LoadingSpinner from "../components/LoadingSpinner";
 import WhitePfp from '../assets/white-placeholder.png';
 import DefaultPfp from '../assets/profile-placeholder.png';
 import { useParams } from 'react-router-dom';
@@ -8,10 +8,11 @@ import axios from 'axios';
 
 
 function PublicPage() {
-  const [profileImage, setProfileImage] = useState(WhitePfp);
-  const timestamp = Date.now();
+  const [profileImage, setProfileImage] = useState(WhitePfp);  // temporary while real pfp loads
+  const [isLoading, setIsLoading] = useState(true);  // tracks loading states for rendering
+  const timestamp = Date.now();  // timestamp is appended to profile picture URL to remove browser caching
   const { userID } = useParams();
-  const [userProfile, setUserProfile] = useState({
+  const [userProfile, setUserProfile] = useState({  // user data
     username: '',
     fullName: '',
     bio: ''
@@ -19,12 +20,8 @@ function PublicPage() {
 
   const fetchUserProfile = async(userID) => {
     try {
-      console.log(process.env.REACT_APP_BACKEND_LINK + `/users/public-profile/${userID}`);
       const response = await axios.get(process.env.REACT_APP_BACKEND_LINK + `/users/public-profile/${userID}`);
-      setUserProfile(response.data);
-      console.log(response.data);
-      console.log(userProfile);
-      //console.log("reached");
+      setUserProfile(response.data);  // update the userProfile data to public profile data retrieved from the backend
     } catch (error) {
       console.log("Error encountered: ", error);
     }
@@ -37,21 +34,27 @@ function PublicPage() {
     } catch (error) {
       console.log("Error encountered: ", error);
     }
-  }
+  }  // if there is no custom pfp set, the profile picture will be set as the default pfp defined in assets
   
   useEffect(() => {
     fetchUserProfile(userID);
     fetchProfilePicture(userID);
+    setIsLoading(false);
   }, []);
 
 
   return (
     <div className = "vertical-center margin padding-top">
-      <div className = "small-container drop-shadow">
-        <h1>{userProfile.fullName}</h1>
-        <img src={profileImage} alt="Profile" className="profile-picture"></img>
-        {userProfile.bio.length > 0 ? <p>{userProfile.bio}</p> : <p>No bio provided.</p>}
-      </div>
+      {isLoading ? (
+        <div><LoadingSpinner/> {/*Visible loading spinner that runs until all data for elements are made availble*/}</div> 
+       ) : (
+          <div className = "small-container drop-shadow">
+            <h1>{userProfile.fullName}</h1>
+            <img src={profileImage} alt="Profile" className="profile-picture"></img>
+            {userProfile.bio.length > 0 ? <p>{userProfile.bio}</p> : <p>No bio provided.</p>}
+          </div>
+      )
+    }
     </div>
   );
 }
