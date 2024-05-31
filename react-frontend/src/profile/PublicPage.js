7b9711a97363276202312cad17a57d6f736bc3f9
jestra03 <joshemestra@gmail.com>
2024-05-30 22:49:27 -0700

    added profile picture to public profile view

--- react-frontend/src/profile/PublicPage.js
+++ react-frontend/src/profile/PublicPage.js
@@ -2,22 +2,24 @@ import React, { useState, useEffect } from 'react';
 import './profile.css';
 //import LoadingSpinner from "../components/LoadingSpinner";
 import WhitePfp from '../assets/white-placeholder.png';
+import DefaultPfp from '../assets/profile-placeholder.png';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 
 function PublicPage() {
-  const [profileImage] = useState(WhitePfp);
+  const [profileImage, setProfileImage] = useState(WhitePfp);
+  const timestamp = Date.now();
   const { userID } = useParams();
   const [userProfile, setUserProfile] = useState({
-    username: '',
-    full_name: '',
-    email: '',
-    phoneNumber: ''
+    username: 'Name',
+    full_name: 'Full Name',
+    bio: 'bio'
   });
 
   useEffect(() => {
     fetchUserProfile(userID);
+    fetchProfilePicture(userID);
   }, []);
 
   const fetchUserProfile = async (userID) => {
@@ -30,10 +32,19 @@ function PublicPage() {
     }
   }
 
+  const fetchProfilePicture = async(userID) => {
+    try {
+      const response = await axios.get(process.env.REACT_APP_BACKEND_LINK + `/users/is-profile-picture/${userID}`);
+      setProfileImage(response ? `https://haggleimgs.s3.amazonaws.com/user/${userID}/bruh0.jpg?${timestamp}` : DefaultPfp);
+    } catch (error) {
+      console.log("Error encountered: ", error);
+    }
+  }
+
   return (
     <div className = "vertical-center margin padding-top">
       <div className = "small-container drop-shadow">
-        <h1>Name</h1>
+        <h1>{userProfile.full_name}</h1>
         <img src={profileImage} alt="Profile" className="profile-picture"></img>
         <p>bio</p>
       </div>
