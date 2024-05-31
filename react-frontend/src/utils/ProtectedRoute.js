b8aad0bbd42c38d37a59c80d919ac6959c22ffd0
jestra03 <joshemestra@gmail.com>
2024-05-31 13:37:22 -0700

    functions file for utils, remove duplicate code

--- react-frontend/src/utils/ProtectedRoute.js
+++ react-frontend/src/utils/ProtectedRoute.js
@@ -1,7 +1,7 @@
 // ProtectedRoute.js
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Navigate} from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({children}) => {
     const isAuthenticated = localStorage.getItem('token') !== null;
