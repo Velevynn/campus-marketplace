36bbe89a47685e5467524f5f311ba9e3ae16e0d1
jestra03 <joshemestra@gmail.com>
2024-06-03 22:24:49 -0700

    PublicPage refactored, clean commented

--- react-frontend/src/profile/PublicPage.js
+++ react-frontend/src/profile/PublicPage.js
@@ -1,24 +1,18 @@
+// PublicPage.js (Joshua Estrada)
+// Displays the public profile data of a given user
 import React, { useState, useEffect } from "react";
-import "./profile.css";
-import LoadingSpinner from "../components/LoadingSpinner";
+import { useParams, useNavigate } from "react-router-dom";
 import WhitePfp from "../assets/white-placeholder.png";
 import DefaultPfp from "../assets/profile-placeholder.png";
-import { useParams } from "react-router-dom";
 import ListingCollection from "./ListingCollection";
+import LoadingSpinner from "../components/LoadingSpinner";
 import ShareButton from "../components/ShareButton";
 import ArrowButton from "../components/ArrowButton";
 import Footer from "../components/Footer";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import "./profile.css";
 
 function PublicPage() {
-	const [profileImage, setProfileImage] = useState(WhitePfp); 
-	const [isLoading, setIsLoading] = useState(true);  
-	const timestamp = Date.now();  
-	const { userID } = useParams();
-	const [listings, setMyListings] = useState([]);
-	const isCustom = false;
-	const navigate = useNavigate();
 	const [userProfile, setUserProfile] = useState({  
 		username: "",
 		fullName: "",
@@ -26,35 +20,42 @@ function PublicPage() {
 		city: ""
 	});
 
+	const navigate = useNavigate();
+	const [profileImage, setProfileImage] = useState(WhitePfp);
+	const [isLoading, setIsLoading] = useState(true); // Loading State
+	const [listings, setMyListings] = useState([]); // myListings State
+	const timestamp = Date.now();  // for removing image caching
+	const { userID } = useParams();  // get userID from the URL params
+	
+	// fetches user profile data from the backend server
 	const fetchUserProfile = async(userID) => {
 		try {
 			const response = await axios.get(process.env.REACT_APP_BACKEND_LINK + `/users/public-profile/${userID}`);
 			setUserProfile(response.data);  
 		} catch (error) {
-			console.log("Error encountered: ", error);
+			console.error("Error encountered: ", error);
 		}
 	};
 
+	// fetches the user's profile picture link from the backend
 	const fetchProfilePicture = async(userID) => {
 		try {
 			const response = await axios.get(process.env.REACT_APP_BACKEND_LINK + `/users/is-profile-picture/${userID}`);
 			setProfileImage(response.data.isProfilePicture ? `https://haggleimgs.s3.amazonaws.com/user/${userID}/bruh0.jpg?${timestamp}` : DefaultPfp);
 		} catch (error) {
-			console.log("Error encountered: ", error);
+			console.error("Error encountered: ", error);
 		}
 	};  
 
+	// fetches the listings owned by the user from the backend
 	const fetchCollections = async (userID) => {
 		try {
-			console.log(userID);
 			const response = await axios.get(process.env.REACT_APP_BACKEND_LINK + `/listings/mylistings/${userID}`);
 			setMyListings(response.data);
-			console.log(response.data, "hello");
-
 		} catch (error) {
 			console.error("Failed to fetch myListings", error);
 		} finally {
-			setIsLoading(false);
+			setIsLoading(false);  // set loading state to false once all data is retrieved
 		}
 	};
 
@@ -62,37 +63,35 @@ function PublicPage() {
 		fetchCollections(userID);
 		fetchUserProfile(userID);
 		fetchProfilePicture(userID);
-	}, []);
-
+	}, []);  // fetch all relevant public user data
 
 	return (
 		<div>
-      
 			{isLoading ? (
 				<div><LoadingSpinner/></div> 
 			) : (
 				<div>
 					<div className = "vertical-center">
-						<div className="vertical-center profile-page-layout margin padding-top">
+						<div className="profile-page-layout margin padding-top">
 							<div className = "small-container drop-shadow">
 								<div className ="full-container">
-									<div className="vertical-center" style={{ display: "flex", alignItems: "center", justifyContent: "center", position: "relative", marginBottom:"15px" }}>
+									<div style={{ display: "flex", alignItems: "center", justifyContent: "center", position: "relative", marginBottom:"15px" }}>
 										<div onClick={() => { navigate(-1); }} style={{ rotate: "-90deg", position: "absolute", left: -30 }}>
-											<ArrowButton></ArrowButton>
+											<ArrowButton/>
 										</div>
 										<h3 style={{ margin: "0 auto" }}>{userProfile.fullName}</h3>
 									</div>
 									<img src={profileImage} alt="Profile" className="profile-picture"></img>
 									<a href={`https://www.google.com/maps/place/${userProfile.city},+CA+93422`} target="_blank" rel="noopener noreferrer">
-										<h5 className="text-link">
+										<h5 className="text-link">  {/*Allows viewer to click on location and see it on Google Maps*/}
 											{userProfile.city && (
 												<div>{userProfile.city}, CA</div>
 											)}
 										</h5>
 									</a>
 									{userProfile.bio.length > 0 ? <p className = "user-bio">{userProfile.bio}</p> : <p>No bio provided.</p>}
-								</div>
-								<ListingCollection title="Listings" bookmarks={listings} userID={userProfile.userID} time = {timestamp} custom = {isCustom} />
+								</div>  {/*If the user did not set a bio, it will default to "No bio provided." */}
+								<ListingCollection title="Listings" bookmarks={listings} userID={userProfile.userID} time = {timestamp} custom = {false} />
 								<div className = "full-container" >
 									<ShareButton link = {`${process.env.REACT_APP_FRONTEND_LINK} + "/profile/" + ${userID}`} type = 'Profile'/>
 								</div>
