4313ab7bb161ad391e41a7a1e5ffa2979499cd71
jestra03 <joshemestra@gmail.com>
2024-06-04 21:13:51 -0700

    works now

--- react-frontend/src/pages/ChatPage.js
+++ react-frontend/src/pages/ChatPage.js
@@ -1,44 +1,45 @@
 // ChatPage.js
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import ChatComponent from '../components/Chat.js';
+import React from "react";
+import {useLocation} from "react-router-dom";
+import ChatComponent from "../components/Chat.js";
 
 function ChatPage() {
-    const { state } = useLocation(); // Using location state to receive listing, seller, and buyer data
-    const listing = state.listing || [];
-    const seller = state.seller || [];
-    const buyer = state.buyer || {};
-    const offer = state.offer || {};
+	const {state} = useLocation(); // Using location state to receive listing, seller, and buyer data
+	const listing = state.listing || [];
+	const seller = state.seller || [];
+	const buyer = state.buyer || {};
+	const offer = state.offer || {};
 
-    console.log({ listing, seller, buyer, offer });
+	console.log({listing, seller, buyer, offer});
 
-    return (
-        <>
-            <div>
-                <h1>Chat for {listing[0]?.title}</h1>
-                <p>Selling by: {seller[0]?.fullName}</p>
-                <p>Offer: {offer}</p>
-                <p>Contacting: {buyer?.fullName}</p>
-            </div>
-            <ChatComponent 
-                appId="tEOH06eA"  // Use your actual TalkJS App ID
-                user={{
-                    id: buyer?.userID || 'default_buyer',
-                    name: buyer?.fullName,
-                    email: buyer?.email,
-                    photoUrl: buyer?.photoUrl,
-                    welcomeMessage: "Hi there, interested in making an offer?"
-                }}
-                otherUser={{
-                    id: seller[0]?.userID || 'default_seller',
-                    name: seller[0]?.fullName,
-                    email: seller[0]?.email,
-                    photoUrl: seller[0]?.photoUrl,
-                    welcomeMessage: "Hello! Feel free to ask any questions you might have."
-                }}
-            />
-        </>
-    );
+	return (
+		<>
+			<div>
+				<h1>Chat for {listing[0]?.title}</h1>
+				<p>Selling by: {seller[0]?.fullName}</p>
+				<p>Offer: {offer}</p>
+				<p>Contacting: {buyer?.fullName}</p>
+			</div>
+			<ChatComponent
+				appId="tEOH06eA" // Use your actual TalkJS App ID
+				user={{
+					id: buyer?.userID || "default_buyer",
+					name: buyer?.fullName,
+					email: buyer?.email,
+					photoUrl: buyer?.photoUrl,
+					welcomeMessage: "Hi there, interested in making an offer?"
+				}}
+				otherUser={{
+					id: seller[0]?.userID || "default_seller",
+					name: seller[0]?.fullName,
+					email: seller[0]?.email,
+					photoUrl: seller[0]?.photoUrl,
+					welcomeMessage:
+						"Hello! Feel free to ask any questions you might have."
+				}}
+			/>
+		</>
+	);
 }
 
 export default ChatPage;
--- react-frontend/src/pages/MakeOfferPage.js
+++ react-frontend/src/pages/MakeOfferPage.js
@@ -1,121 +1,145 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Form, OfferGroup, Offer, HeaderLabel, MakeOfferButton } from '../authentication/AuthenticationStyling';
-import { useNavigate, useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, {useState, useEffect} from "react";
+import {
+	Container,
+	Form,
+	OfferGroup,
+	Offer,
+	HeaderLabel,
+	MakeOfferButton
+} from "../authentication/AuthenticationStyling.js";
+import {useNavigate, useParams} from "react-router-dom";
+import axios from "axios";
 
 function MakeOfferPage() {
-  console.log("setup");
-  const [offer, setOffer] = useState("0");
-  const [buyer, setBuyer] = useState(null);
-  const navigate = useNavigate();
-  const { listingID } = useParams();
+	console.log("setup");
+	const [offer, setOffer] = useState("0");
+	const [buyer, setBuyer] = useState(null);
+	const navigate = useNavigate();
+	const {listingID} = useParams();
 
-  useEffect(() => {
-    console.log("use effect");
-    const fetchUserProfile = async () => {
-      const token = localStorage.getItem('token');
-      if (!token) {
-        navigate('/login');
-        return;
-      }
-      console.log("start call 1");
-      try {
-        const response = await axios.get(`${process.env.REACT_APP_BACKEND_LINK}/users/profile`, {
-          headers: { 'Authorization': `Bearer ${token}` },
-        });
-        setBuyer(response.data);
-      } catch (error) {
-        console.error('Failed to fetch user data:', error);
-        navigate('/login');
-      }
-      console.log("end call 1");
-    };
+	useEffect(() => {
+		console.log("use effect");
+		const fetchUserProfile = async () => {
+			const token = localStorage.getItem("token");
+			if (!token) {
+				navigate("/login");
+				return;
+			}
+			console.log("start call 1");
+			try {
+				const response = await axios.get(
+					`${process.env.REACT_APP_BACKEND_LINK}/users/profile`,
+					{
+						headers: {Authorization: `Bearer ${token}`}
+					}
+				);
+				setBuyer(response.data);
+			} catch (error) {
+				console.error("Failed to fetch user data:", error);
+				navigate("/login");
+			}
+			console.log("end call 1");
+		};
 
-    fetchUserProfile();
-  }, [navigate]);
+		fetchUserProfile();
+	}, [navigate]);
 
-  const handleMakeOffer = async () => {
-    console.log("start handleMakeOffer");
-    if (!buyer) {
-      console.error("Buyer information is not available.");
-      return;
-    }
-    console.log("get token");
-    const token = localStorage.getItem("token");
+	const handleMakeOffer = async () => {
+		console.log("start handleMakeOffer");
+		if (!buyer) {
+			console.error("Buyer information is not available.");
+			return;
+		}
+		console.log("get token");
+		const token = localStorage.getItem("token");
 
-    try {
-      console.log("listingResponse");
-      const listingResponse = await axios.get(process.env.REACT_APP_BACKEND_LINK + `/listings/${listingID}`, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-      const listing = listingResponse.data;
-      console.log(listing);
+		try {
+			console.log("listingResponse");
+			const listingResponse = await axios.get(
+				process.env.REACT_APP_BACKEND_LINK + `/listings/${listingID}`,
+				{
+					headers: {
+						Authorization: `Bearer ${token}`
+					}
+				}
+			);
+			const listing = listingResponse.data;
+			console.log(listing);
 
-      console.log("sellerResponse: ", listing[0].userID);
-      console.log(process.env.REACT_APP_BACKEND_LINK + `/users/${listing[0].userID}`);
-      const sellerResponse = await axios.get(process.env.REACT_APP_BACKEND_LINK + `/users/${listing[0].userID}`, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-      const seller = sellerResponse.data;
-      console.log(seller);
-  
-      navigate(`/chat`, { state: { listing, seller, buyer, offer } });
-    } catch (error) {
-      console.error('Error making offer:', error);
-    }
-  };
+			console.log("sellerResponse: ", listing[0].userID);
+			console.log(
+				process.env.REACT_APP_BACKEND_LINK +
+					`/users/${listing[0].userID}`
+			);
+			const sellerResponse = await axios.get(
+				process.env.REACT_APP_BACKEND_LINK +
+					`/users/${listing[0].userID}`,
+				{
+					headers: {
+						Authorization: `Bearer ${token}`
+					}
+				}
+			);
+			const seller = sellerResponse.data;
+			console.log(seller);
 
-  const handleInputChange = (event) => {
-    const { value } = event.target;
-    const numbers = value.replace(/[^0-9]/g, '');
-    setOffer(numbers);
-};
+			navigate(`/chat`, {state: {listing, seller, buyer, offer}});
+		} catch (error) {
+			console.error("Error making offer:", error);
+		}
+	};
 
-  const formatNumber = (number) => {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 0
-    }).format(number);
-  };
+	const handleInputChange = event => {
+		const {value} = event.target;
+		const numbers = value.replace(/[^0-9]/g, "");
+		setOffer(numbers);
+	};
 
-  const handleGoBack = () => {
-    navigate(`/listings/${listingID}`);
-  };
+	const formatNumber = number => {
+		return new Intl.NumberFormat("en-US", {
+			style: "currency",
+			currency: "USD",
+			minimumFractionDigits: 0,
+			maximumFractionDigits: 0
+		}).format(number);
+	};
 
-  return (
-    <>
-      <Container>
-        <HeaderLabel style={{ marginTop: '0px' }}>
-          Enter Your Offer
-        </HeaderLabel>
-        <Form onSubmit={(event) => event.preventDefault()}>
-          <OfferGroup>
-            <Offer
-              type="text"
-              name="identifier"
-              id="identifier"
-              required
-              value={formatNumber(offer)}
-              onChange={handleInputChange}
-              maxLength="10"
-            />
-          </OfferGroup>
-          <MakeOfferButton onClick={handleMakeOffer}>
-            Make Offer
-          </MakeOfferButton>
-          <MakeOfferButton type="button" onClick={handleGoBack} style={{marginTop: "-10px"}}>
-            Return to Listing
-          </MakeOfferButton>
-        </Form>
-      </Container>
-    </>
-  );
+	const handleGoBack = () => {
+		navigate(`/listings/${listingID}`);
+	};
+
+	return (
+		<>
+			<Container>
+				<HeaderLabel style={{marginTop: "0px"}}>
+					Enter Your Offer
+				</HeaderLabel>
+				<Form onSubmit={event => event.preventDefault()}>
+					<OfferGroup>
+						<Offer
+							type="text"
+							name="identifier"
+							id="identifier"
+							required
+							value={formatNumber(offer)}
+							onChange={handleInputChange}
+							maxLength="10"
+						/>
+					</OfferGroup>
+					<MakeOfferButton onClick={handleMakeOffer}>
+						Make Offer
+					</MakeOfferButton>
+					<MakeOfferButton
+						type="button"
+						onClick={handleGoBack}
+						style={{marginTop: "-10px"}}
+					>
+						Return to Listing
+					</MakeOfferButton>
+				</Form>
+			</Container>
+		</>
+	);
 }
 
 export default MakeOfferPage;
