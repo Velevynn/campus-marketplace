5b41ec49a6dc6f1fc71aa39b4df2b8631eb50630
realkaranvir <113927390+realkaranvir@users.noreply.github.com>
2024-06-05 12:26:16 -0700

    fixed compile error

--- react-frontend/.eslintrc.js
+++ react-frontend/.eslintrc.js
@@ -1,53 +1,45 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
+	env: {
+		browser: true,
+		es2021: true
 	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:react/recommended"
-	],
-	"overrides": [
+	extends: ["eslint:recommended", "plugin:react/recommended"],
+	overrides: [
 		{
-			"env": {
-				"node": true
+			env: {
+				node: true
 			},
-			"files": [
-				".eslintrc.{js,cjs}"
-			],
-			"parserOptions": {
-				"sourceType": "script"
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script"
 			}
 		}
 	],
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module"
 	},
-	"plugins": [
-		"react"
-	],
-	"rules": {
+	plugins: ["react"],
+	rules: {
 		"no-console": "off",
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"windows"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		],
-		"no-mixed-spaces-and-tabs": [
-			"error", 
-			"smart-tabs"
-		],
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+		"no-mixed-spaces-and-tabs": ["error", "smart-tabs"]
+	},
+	ignorePatterns: ["node_modules/", "dist/"],
+	globals: {
+		jQuery: "readonly",
+		$: "readonly",
+		React: "readonly",
+		ReactDOM: "readonly",
+		express: "readonly",
+		indexing_v3: "readonly",
+		multer: "readonly",
+		upload: "readonly",
+		router: "readonly",
+		Pool: "readonly",
+		dotenv: "readonly"
 	}
 };
