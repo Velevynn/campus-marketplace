c7fb3ec6b470d5fb2122d5f73f1cab28eafc01a4
jestra03 <joshemestra@gmail.com>
2024-06-05 12:50:11 -0700

    fix

--- react-frontend/src/profile/PublicPage.js
+++ react-frontend/src/profile/PublicPage.js
@@ -13,7 +13,7 @@ import axios from "axios";
 import "./profile.css";
 
 function PublicPage() {
-	const [userProfile, setUserProfile] = useState({  
+	const [userProfile, setUserProfile] = useState({
 		username: "",
 		fullName: "",
 		bio: "",
@@ -26,26 +26,26 @@ function PublicPage() {
 	const [listings, setMyListings] = useState([]); // myListings State
 	const timestamp = Date.now();  // for removing image caching
 	const { userID } = useParams();  // get userID from the URL params
-	
+
 	// fetches user profile data from the backend server
-	const fetchUserProfile = async(userID) => {
+	const fetchUserProfile = async (userID) => {
 		try {
 			const response = await axios.get(process.env.REACT_APP_BACKEND_LINK + `/users/public-profile/${userID}`);
-			setUserProfile(response.data);  
+			setUserProfile(response.data);
 		} catch (error) {
 			console.error("Error encountered: ", error);
 		}
 	};
 
 	// fetches the user's profile picture link from the backend
-	const fetchProfilePicture = async(userID) => {
+	const fetchProfilePicture = async (userID) => {
 		try {
 			const response = await axios.get(process.env.REACT_APP_BACKEND_LINK + `/users/is-profile-picture/${userID}`);
 			setProfileImage(response.data.isProfilePicture ? `https://haggleimgs.s3.amazonaws.com/user/${userID}/bruh0.jpg?${timestamp}` : DefaultPfp);
 		} catch (error) {
 			console.error("Error encountered: ", error);
 		}
-	};  
+	};
 
 	// fetches the listings owned by the user from the backend
 	const fetchCollections = async (userID) => {
@@ -68,16 +68,16 @@ function PublicPage() {
 	return (
 		<div>
 			{isLoading ? (
-				<div><LoadingSpinner/></div> 
+				<div><LoadingSpinner /></div>
 			) : (
 				<div>
-					<div className = "vertical-center">
+					<div className="vertical-center">
 						<div className="profile-page-layout margin padding-top">
-							<div className = "small-container drop-shadow">
-								<div className ="full-container">
-									<div style={{ display: "flex", alignItems: "center", justifyContent: "center", position: "relative", marginBottom:"15px" }}>
+							<div className="small-container drop-shadow">
+								<div className="full-container">
+									<div style={{ display: "flex", alignItems: "center", justifyContent: "center", position: "relative", marginBottom: "15px" }}>
 										<div onClick={() => { navigate(-1); }} style={{ rotate: "-90deg", position: "absolute", left: -30 }}>
-											<ArrowButton/>
+											<ArrowButton />
 										</div>
 										<h3 style={{ margin: "0 auto" }}>{userProfile.fullName}</h3>
 									</div>
@@ -89,16 +89,16 @@ function PublicPage() {
 											)}
 										</h5>
 									</a>
-									{userProfile.bio.length > 0 ? <p className = "user-bio">{userProfile.bio}</p> : <p>No bio provided.</p>}
+									{userProfile.bio.length > 0 ? <p className="user-bio">{userProfile.bio}</p> : <p>No bio provided.</p>}
 								</div>  {/*If the user did not set a bio, it will default to "No bio provided." */}
-								<ListingCollection title="Listings" bookmarks={listings} userID={userProfile.userID} time = {timestamp} custom = {false} />
-								<div className = "full-container" >
-									<ShareButton link = {`${process.env.REACT_APP_FRONTEND_LINK} + "/profile/" + ${userID}`} type = "Profile"/>
+								<ListingCollection title="Listings" bookmarks={listings} userID={userProfile.userID} time={timestamp} custom={false} />
+								<div className="full-container" >
+									<ShareButton link={`${process.env.REACT_APP_FRONTEND_LINK} + "/profile/" + ${userID}`} type="Profile" />
 								</div>
 							</div>
 						</div>
 					</div>
-					<Footer/>
+					<Footer />
 				</div>
 			)}
 		</div>
