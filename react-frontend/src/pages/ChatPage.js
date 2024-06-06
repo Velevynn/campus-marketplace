412e3a5ec00ecad07711cc38313acaf2cb0a80d8
jestra03 <joshemestra@gmail.com>
2024-06-06 15:51:34 -0700

    fix direct chat

--- react-frontend/src/pages/ChatPage.js
+++ react-frontend/src/pages/ChatPage.js
@@ -4,7 +4,12 @@ import {useLocation} from "react-router-dom";
 import ChatComponent from "../components/Chat.js";
 
 function ChatPage() {
+	//const isLoading = false;
 	const {state} = useLocation(); // Using location state to receive listing, seller, and buyer data
+	if (state === null) {
+		window.location.href = "/not-found";
+		return <></>;
+	}
 	const listing = state.listing || [];
 	const seller = state.seller || [];
 	const buyer = state.buyer || {};
@@ -14,30 +19,35 @@ function ChatPage() {
 
 	return (
 		<>
-			<div>
-				<h1>Chat for {listing[0]?.title}</h1>
-				<p>Selling by: {seller[0]?.fullName}</p>
-				<p>Offer: {offer}</p>
-				<p>Contacting: {buyer?.fullName}</p>
-			</div>
-			<ChatComponent
-				appId="tEOH06eA" // Use your actual TalkJS App ID
-				user={{
-					id: buyer?.userID || "default_buyer",
-					name: buyer?.fullName,
-					email: buyer?.email,
-					photoUrl: buyer?.photoUrl,
-					welcomeMessage: "Hi there, interested in making an offer?"
-				}}
-				otherUser={{
-					id: seller[0]?.userID || "default_seller",
-					name: seller[0]?.fullName,
-					email: seller[0]?.email,
-					photoUrl: seller[0]?.photoUrl,
-					welcomeMessage:
-						"Hello! Feel free to ask any questions you might have."
-				}}
-			/>
+			{state !== null && (
+				<div>
+					<div>
+						<h1>Chat for {listing[0]?.title}</h1>
+						<p>Selling by: {seller[0]?.fullName}</p>
+						<p>Offer: {offer}</p>
+						<p>Contacting: {buyer?.fullName}</p>
+					</div>
+					<ChatComponent
+						appId="tEOH06eA" // Use your actual TalkJS App ID
+						user={{
+							id: buyer?.userID || "default_buyer",
+							name: buyer?.fullName,
+							email: buyer?.email,
+							photoUrl: buyer?.photoUrl,
+							welcomeMessage:
+								"Hi there, interested in making an offer?"
+						}}
+						otherUser={{
+							id: seller[0]?.userID || "default_seller",
+							name: seller[0]?.fullName,
+							email: seller[0]?.email,
+							photoUrl: seller[0]?.photoUrl,
+							welcomeMessage:
+								"Hello! Feel free to ask any questions you might have."
+						}}
+					/>
+				</div>
+			)}
 		</>
 	);
 }
