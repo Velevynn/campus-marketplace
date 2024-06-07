1042b4b1cb0730ce27637228d1c86b6bc8a77ab2
jcromeen <jimmy.cromeenes@gmail.com>
2024-06-07 05:45:03 -0700

    chat page

--- react-frontend/src/components/Chat.js
+++ react-frontend/src/components/Chat.js
@@ -1,65 +1,65 @@
-import React, { useEffect, useRef } from "react";
+import React, {useEffect, useRef} from "react";
 import Talk from "talkjs";
 import PropTypes from "prop-types";
-import "./Chat.css"; 
+import "./Chat.css";
 
-function ChatComponent({ appId, user, otherUser }) {
-    const chatContainerRef = useRef();
+function ChatComponent({appId, user, otherUser}) {
+	const chatContainerRef = useRef();
 
-    useEffect(() => {
-        if (!Talk.ready) return;
+	useEffect(() => {
+		if (!Talk.ready) return;
 
-        Talk.ready.then(() => {
-            const me = new Talk.User(user);
-            const other = new Talk.User(otherUser);
+		Talk.ready.then(() => {
+			const me = new Talk.User(user);
+			const other = new Talk.User(otherUser);
 
-            const session = new Talk.Session({
-                appId: appId,
-                me: me
-            });
+			const session = new Talk.Session({
+				appId: appId,
+				me: me
+			});
 
-            const conversationId = Talk.oneOnOneId(me, other);
-            const conversation = session.getOrCreateConversation(conversationId);
+			const conversationId = Talk.oneOnOneId(me, other);
+			const conversation =
+				session.getOrCreateConversation(conversationId);
 
-            conversation.setParticipant(me);
-            conversation.setParticipant(other);
+			conversation.setParticipant(me);
+			conversation.setParticipant(other);
 
-            const chatbox = session.createChatbox();
-            chatbox.select(conversation);
-            chatbox.mount(chatContainerRef.current);
-        });
+			const chatbox = session.createChatbox();
+			chatbox.select(conversation);
+			chatbox.mount(chatContainerRef.current);
+		});
 
-        return () => {
-            if (chatContainerRef.current) {
-                chatContainerRef.current.innerHTML = "";
-            }
-        };
-    }, [appId, user, otherUser]);
+		return () => {
+			if (chatContainerRef.current) {
+				chatContainerRef.current.innerHTML = "";
+			}
+		};
+	}, [appId, user, otherUser]);
 
-    return (
-        <div className="chat-container">
-            <div className="header">Chat with {otherUser.name}</div>
-            <div ref={chatContainerRef} className="body"></div>
-        </div>
-    );
+	return (
+		<div className="chat-container">
+			<div ref={chatContainerRef} className="body"></div>
+		</div>
+	);
 }
 
 ChatComponent.propTypes = {
-    appId: PropTypes.string.isRequired,
-    user: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        email: PropTypes.string,
-        photoUrl: PropTypes.string,
-        welcomeMessage: PropTypes.string
-    }).isRequired,
-    otherUser: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        email: PropTypes.string,
-        photoUrl: PropTypes.string,
-        welcomeMessage: PropTypes.string
-    }).isRequired
+	appId: PropTypes.string.isRequired,
+	user: PropTypes.shape({
+		id: PropTypes.string.isRequired,
+		name: PropTypes.string.isRequired,
+		email: PropTypes.string,
+		photoUrl: PropTypes.string,
+		welcomeMessage: PropTypes.string
+	}).isRequired,
+	otherUser: PropTypes.shape({
+		id: PropTypes.string.isRequired,
+		name: PropTypes.string.isRequired,
+		email: PropTypes.string,
+		photoUrl: PropTypes.string,
+		welcomeMessage: PropTypes.string
+	}).isRequired
 };
 
 export default ChatComponent;
--- react-frontend/src/pages/ChatPage.js
+++ react-frontend/src/pages/ChatPage.js
@@ -1,34 +1,65 @@
 // ChatPage.js
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import {useLocation} from "react-router-dom";
 import ChatComponent from "../components/Chat.js";
+import missing from "../assets/missing.jpg";
 
 function ChatPage() {
 	//const isLoading = false;
 	const {state} = useLocation(); // Using location state to receive listing, seller, and buyer data
+	const [imageSource, setImageSource] = useState(missing);
+
+	useEffect(() => {
+		const fetchImages = async () => {
+			try {
+				if (state === null || !state.listing || !state.listing[0])
+					return;
+				const response = await axios.get(
+					process.env.REACT_APP_BACKEND_LINK +
+						`/listings/images/${state.listing[0].listingID}`
+				);
+				if (response.data.length > 0) {
+					setImageSource(
+						`https://haggleimgs.s3.amazonaws.com/${state.listing[0].listingID}/image0`
+					);
+				}
+			} catch (error) {
+				console.error("Error fetching images:", error);
+			}
+		};
+		fetchImages();
+	}, [state]);
+
 	if (state === null) {
 		window.location.href = "/not-found";
 		return <></>;
 	}
+
 	const listing = state.listing || [];
 	const seller = state.seller || [];
 	const buyer = state.buyer || {};
 	const offer = state.offer || {};
 
 	console.log({listing, seller, buyer, offer});
+	console.log(listing[0]);
 
 	return (
 		<>
 			{state !== null && (
-				<div>
-					<div>
-						<h1>Chat for {listing[0]?.title}</h1>
-						<p>Selling by: {seller[0]?.fullName}</p>
+				<div
+					className="vertical-center margin"
+					style={{marginTop: "35px"}}
+				>
+					<div className="small-container">
+						<img src={imageSource} alt="Listing" />
+						<h1>{listing[0]?.title}</h1>
+						<p>{seller[0]?.fullName}</p>
 						<p>Offer: {offer}</p>
 						<p>Contacting: {buyer?.fullName}</p>
 					</div>
 					<ChatComponent
-						appId={ process.env.REACT_APP_TALKJS_APP_ID } // Use your actual TalkJS App ID
+						appId={process.env.REACT_APP_TALKJS_APP_ID} // Use your actual TalkJS App ID
 						user={{
 							id: buyer?.userID || "default_buyer",
 							name: buyer?.fullName,
